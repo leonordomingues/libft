73a0a7ef12336b8805bc9bcbde454a92a2e7b41f
Leonor Domingues <leonordomingues@unknownb0be7645f414.lan>
2022-09-23 22:18:09 +0100

    T2

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,9 +3,13 @@
 char	*ft_strnstr(const char *h, const char *n, size_t len)
 {
 	int	i;
+	int	a;
 
 	i = 0;
+	a = 0;
 	if (n == 0)
 		return (h);
 	
-	while (h[i] != '\0' 
+	while (i < len || h[i] != '\0' || n[a] != '\0')
+	{
+		if (n[a] ==
