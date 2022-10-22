0e717003490d3f9263bed61638c396c924b1179f
Leonor Domingues <leonordomingues@MacBook-Pro-de-Leonor.local>
2022-10-22 19:42:42 +0100

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,11 +1,54 @@
 #include "libft.h"
 
+static int	intlen(int num)
+{
+	int	i;
+
+	i = 0;
+	if (num < 0)
+	{
+		num *= -1;
+		i++;
+	}
+	else if (num == 0)
+		return (1);
+	while (num > 0)
+	{
+		num = num / 10;
+		i++;
+	}
+	return (i);
+}
+
 char	*ft_itoa(int n)
 {
 	int	i;
+	int	count;
 	char	*str;
 
-	i = 0;
+	count = intlen(n);
+	str = (char *)malloc(sizeof(char) * count + 1);
+	if (!str)
+		return (NULL);
 	if (n < 0)
-		str[i] = '-';
+	{
+		str[0] = '-';
+		n *= -1;
+	}
+	else if(n == 0)
+		str[0] = '0';
+	str[count] = '\0';
+	while (n > 0)
+	{
+		str[--count] = n % 10 + '0';
+		n /= 10;
+	}
+	return (str);
+}
 
+/*int	main()
+{
+	int	n = 0;
+	char	*str = ft_itoa(n);
+	printf("%s\n", str);
+}*/
--- libft.h
+++ libft.h
@@ -32,7 +32,7 @@ char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strtrim(const char *s1, const char *set);
 //ft_split();
-//ft_itoa();
+char	*ft_itoa(int n);
 char	*ft_strmapi(const char *s, char (*f) (unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
