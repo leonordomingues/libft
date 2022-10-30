250eee8de053630b3d23721869b51df877152d17
Leonor Domingues <leonordomingues@MBP-de-Leonor.lan>
2022-10-30 01:28:30 +0100

    iteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -17,15 +17,14 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 	int	i;
 	int	len;
 
+	if (!s || !f)
+		return ;
 	i = 0;
 	len = ft_strlen(s);
-	if (s != NULL && f != NULL)
+	while (i < len)
 	{
-		while (i < len)
-		{
-			(*f)(i, s);
-			s++;
-			i++;
-		}
+		(*f)(i, s);
+		s++;
+		i++;
 	}
 }
